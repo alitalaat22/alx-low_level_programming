23e79468f6577b072887b9a2c08f5aefee03bace
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-13 19:50:21 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-//printv some worled
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main()
 {
 printf ("with proper grammar, but the outcome is a piece of art,");
