cd20da359a403500147592dcdc6cdbff792423dd
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-16 12:49:42 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,7 @@
 *
 *description print all single digit numbers of base 10 starting from 0
 *
-*return: always  0 (success)
+*Return: always  0 (success)
 */
 
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 
 while (digt <= 9)
 {
-/*convert digit to ASCII representation*/
+/*convert digt to ASCII representation*/
 putchar(digt + '0');
 digt++;
 }
