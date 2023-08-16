1a0abbb5de0ce322027621ff39280eda280316aa
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-15 19:54:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * min entry point
+ *
+ * print all alphabit letters
+ *
+ * return always 0 success
+ */
+
+int main(void)
+{
+char d = 'a';
+while (d <= 'z')
+{
+printf("%d\n", d);
+d++;
+}
+	return (0);
+}
