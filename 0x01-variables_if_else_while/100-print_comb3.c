b0f1cfc18281e7f0a4a0f3c61a89942ba044ad0b
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-16 15:43:51 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+*main entry point
+*
+* description prints all possible different combinations of two digits.
+*
+* Return Always 0 (suxxess)
+*/
+int main(void)
+{
+int digo = 0, digos;
+while (digo <= 9)
+{
+digos = 0;
+while (digos <= 9)
+{
+if (digo != digos && digo < digos)
+{
+putchar(digo + 48);
+putchar(digos + 48);
+
+if (digo + digos != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+digos++;
+}
+digo++;
+}
+putchar('\n');
+return (0);
+}
