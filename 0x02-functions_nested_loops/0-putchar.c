40012b9bff477c76c8734866964fa0fc4259e6aa
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-16 18:28:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,16 @@
 */
 int main(void)
 {
-int _putchar;
-printf("_putchar\n");
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+}
 return (0);
 }
