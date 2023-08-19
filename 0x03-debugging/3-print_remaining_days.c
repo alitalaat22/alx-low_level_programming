7330c7ae5f58a8c9daa75c2c4161d5a797ca4942
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-19 14:34:36 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,7 +27,7 @@ else
 if (month == 2 && day == 60)
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-{
+}
 else
 {
 printf("Day of the year: %d\n", day);
@@ -35,4 +35,3 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-
