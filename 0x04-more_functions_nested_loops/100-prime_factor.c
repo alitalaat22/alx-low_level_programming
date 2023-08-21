5c0df465a627be7e7cb586f82a57785a1ce8bdf4
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-21 00:00:07 -0700

    finish

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,27 +6,27 @@
  */
 int main(void)
 {
-long n = 612852475143;
-long i = 2;
-long max = 0;
-do {
-while (i <= n)
-{
-if ((n % i) == 0)
-{
-n = n / i;
-if (i > max)
-{
-max = i;
-}
-}
-else
-{
-i++;
-}
-}
-}
-while (i <= n);
-printf("%ld\n", max);
-return (0);
+	long n = 612852475143;
+	long i = 2;
+	long max = 0;
+
+	do {
+		while (i <= n)
+		{
+			if ((n % i) == 0)
+			{
+				n = n / i;
+				if (i > max)
+				{
+					max = i;
+				}
+			}
+			else
+			{
+				i++;
+			}
+		}
+	} while (i <= n);
+	printf("%ld\n", max);
+	return (0);
 }
