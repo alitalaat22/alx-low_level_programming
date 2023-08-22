ed880166bee361661b4187947f0a6986fa898f6c
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-21 22:24:55 -0700

    main .h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *t);
+void swap_int(int *g, int *h);
+int _strlen(char *STR);
+void _puts(char *STR);
+void print_rev(char *STR);
+void rev_string(char *STR);
+void puts2(char *STR);
+void puts_half(char *STR);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
