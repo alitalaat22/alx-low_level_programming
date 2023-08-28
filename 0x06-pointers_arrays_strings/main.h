a72e8f82c7c5ccf7e4b8841d97390743eed9b364
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-28 04:44:46 -0700

    advanced 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,27 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-/**
- * main - main function
- * Return: int value
- * _putchar - prints character in the  output
- * *_strcat -  appends the src string to the dest string
- * @*src:the string pointed to by src
- * @*dest: destination pointer
- * *_strncat -  appends the src string to the dest string
- * *_strncpy -  copies a string
- * _strcmp - compares two strings
- * @*s1: first string
- * @*s2: second string
- * @*a: resversed array
- * *string_toupper - changes all lowercase letters of a string to uppercase
- * *cap_string - capitalizes all words of a string
- * *leet - encode a string
- * *rot13 - encodes a string using rot13
- */
 
-int main(void);
-int _putchar(char ch);
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -33,5 +14,9 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
 
 #endif
