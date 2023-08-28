57a2ee67c2826270ed4e75e59a9f26e2bcaf67b7
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-28 14:44:56 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 int x;
 int y;
-for (x = 0; dest[x] != '\0'; x++)
 
+for (x = 0; dest[x] != '\0'; x++)
+;
 for (y = 0; y < n && src[y] != '\0'; y++)
 
 dest[x + y] = src[y];
