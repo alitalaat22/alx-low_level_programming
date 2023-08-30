bb84c812f6f3d22a64e48234e2c1eb402623b21c
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-30 10:10:27 -0700

    tak 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_puts_recursion - print string
+*
+*@s: the crachter to print string
+*
+*Return: void
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_puthar(*s);
+}
+{
+_puts_recursion(++s);
+}
+}
