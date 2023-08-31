7f5edc2a8ce65810cce7b488305c4cccdbcc4e1d
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-30 17:35:20 -0700

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include "main.h"
+/**
+*_print_rev_recursion -  reverse
+*
+*@s: reverse
+*
+* Return: 0
+*/
+
+void _print_rev_recursion(char *s);
+{
+if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
