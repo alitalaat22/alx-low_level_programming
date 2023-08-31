424bc8f9564c99f1bae5ce214336bdff07ab0e11
alitalaat22 <elshaaerali16@gmail.com.com>
2023-08-30 17:43:36 -0700

    klo

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,13 @@
-#include <stdio>
 #include "main.h"
-/**
-*_print_rev_recursion -  reverse
-*
-*@s: reverse
-*
-* Return: 0
-*/
 
-void _print_rev_recursion(char *s);
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
+	if (*s == '\0')
 		return;
 
 	_print_rev_recursion(s + 1);
