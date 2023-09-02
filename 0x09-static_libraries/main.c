10ab71c674fd0d971c65cd69a3d703c326d850f5
alitalaat22 <elshaaerali16@gmail.com.com>
2023-09-02 14:54:29 -0700

    lib

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-*main - return void
+*main - void
 *
 *Return: 0
 */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
-- Kevin Mitnick");
+_puts("\"At the end of the day,my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
